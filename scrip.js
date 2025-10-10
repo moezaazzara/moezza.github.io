df7ab39090b34af2c15f6c587f3083a6f588e0a2
Adhyyyyyy <Dhynurrohman@gmail.com>
2025-10-10 19:12:38 +0700

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,14 @@
+alert("assalamualaikum")
+    console.log("scrip.js internal berhasil")
+
+function sapa(nama){
+    let pesan = "halo," + nama + "! apa kabar?";
+    return pesan;
+
+let ucapanuntukbudi =  sapa("budi");
+console.log(ucapanuntukbudi);
+
+let ucapanuntukani = sapa("ani")
+console.log(ucapanuntukani);
+
+}
