3797d9e63087a8037e654d9f1a214513f30b430d
Adhyyyyyy <Dhynurrohman@gmail.com>
2025-10-10 20:02:01 +0700

    script

--- scrip.js
+++ scrip.js
@@ -10,3 +10,22 @@ console.log(ucapanuntukbudi);
 
 let ucapanuntukani = sapa("ani")
 console.log(ucapanuntukani);
+
+const judul=document.getElementById('judul-utama');
+console.log(judul);
+
+const infopertama=document.querySelector('.info');
+console.log(infopertama);
+
+const semuainfo=document.querySelectorAll('.info');
+console.log(semuainfo);
+
+judul.innerText= "selamat datang di dunia DOM";
+
+const divinfo = document.querySelector('div.info');
+divinfo.innerHTML = "<strong>teks ini sekarang tebal!</strong>";
+
+judul.style.color = 'blue';
+judul.style.backgroundColor = 'lightgray';
+judul.style.padding = '15px';
+judul.style.textAlign = 'center';
